b50a8b7dfd9c467bae21569e483a2b3941d86d96
bhead <konstantin.kostenko@inplatlabs.ru>
2023-03-16 20:12:34 +0300

    removed role class

--- api_yamdb/accounts/models.py
+++ api_yamdb/accounts/models.py
@@ -2,17 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class UserRoles(models.TextChoices):
-    ADMIN = "admin", "Admin"
-    USER = "user", "User"
-    MODERATOR = "moderator", "Moderator"
-
-
 class User(AbstractUser):
+    """Custom user model."""
+    USER_ROLE = (
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin'),
+    )
     role = models.CharField(
         max_length=20,
-        choices=UserRoles.choices,
-        default=UserRoles.USER,
+        choices=USER_ROLE,
+        default='user',
     )
     email = models.EmailField(
         max_length=255,
@@ -34,15 +34,15 @@ class User(AbstractUser):
 
     @property
     def is_user(self):
-        return self.role == UserRoles.USER
+        return self.role == 'user'
 
     @property
     def is_admin(self):
-        return self.role == UserRoles.ADMIN
+        return self.role == 'admin'
 
     @property
     def is_moderator(self):
-        return self.role == UserRoles.MODERATOR
+        return self.role == 'moderator'
 
     def __str__(self):
         return self.username
